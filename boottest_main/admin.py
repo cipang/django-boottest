7327fe331d9c45d270682c49ae068ba3ad1293ac
cipang <mail@patrickpang.net>
2015-09-27 00:30:23 +1000

    Added TestRecord to admin UI.

--- boottest_main/admin.py
+++ boottest_main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TestRecord
 
-# Register your models here.
+
+@admin.register(TestRecord)
+class TestRecordAdmin(admin.ModelAdmin):
+    list_display = ("id", "num_value", "text_value", "created")
